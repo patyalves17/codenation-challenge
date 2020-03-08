e5d0d2551ad4dbb334a60903a3eae8f42c9a0142
patyalves17 <patriciaalves10@gmail.com>
2020-03-07 23:22:30 -0300

    get from api and decrypt

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('ta funfando');
+const axios = require('axios');
+const token = require ('./token');
+const fs = require('fs');
+var SHA1 = require("crypto-js/sha1");
+
+
+    axios.get(`https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=${token}`)
+        .then(resp =>{
+            console.log(resp.data);
+            // fs.writeFileSync('answer.json',JSON.stringify(resp.data) )
+            resp.data.decifrado =  decrypt(resp.data);
+            console.log( resp.data);
+            // fs.writeFileSync('answer.json',JSON.stringify(resp.data) )
+            
+            
+        })
+        .catch(error =>{
+            console.log(error);
+        });
+
+
+const decrypt=(data) =>{
+     [...data.cifrado].map(element => {
+          const codeAt = element.charCodeAt(0);
+          let aux = element;    
+          if(codeAt >=97 && codeAt<=122){
+            aux = String.fromCharCode(codeAt-data.numero_casas);
+          }
+          data.decifrado +=aux;
+          
+      });
+
+      return data.decifrado;
+}
---
+++ token.js
@@ -0,0 +1,3 @@
+const token = 'b1a8fcd4628c6710df47ed1bb9366272270f8102';
+
+module.exports = token;
